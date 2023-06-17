d867860625663b4312cc817a23aafae374b4721d
Arvind <thakurarvindkr10@gmail.com>
2023-06-17 09:03:13 +0530

    I have added project which would show news and products of ecommerce also

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import News from './components/News.jsx'
+import Product from './components/Product.jsx'
+
+const App = () => {
+  return (
+      <BrowserRouter>
+          <Routes>
+              <Route></Route>
+          </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
